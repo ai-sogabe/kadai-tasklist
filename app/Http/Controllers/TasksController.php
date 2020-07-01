d1e571c589f4259209cfbf20ec4ed99e3cc0c7cf
ai.sogabe <ftsuki0622@gmail.com>
2020-07-01 08:24:05 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -44,8 +44,15 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        // バリデーション
+        $this->validate($request, [
+            'status' => 'required|max:10',
+            
+        ]);
+        
         // タスクを作成
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
@@ -96,9 +103,16 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        // バリデーション
+        $this->validate($request, [
+            'status' => 'required|max:10',
+            
+        ]);
+        
         // idの値でタスクを検索して取得
         $task = Task::findOrFail($id);
         // メッセージを更新
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
